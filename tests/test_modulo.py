0f9ab36a358814dfb793146d925526dbb26c77c9
isciaberger <158447780+isciaberger@users.noreply.github.com>
2025-08-04 12:54:12 +0000

    Added Messages

--- tests/test_modulo.py
+++ tests/test_modulo.py
@@ -3,5 +3,10 @@ from src.utils import modulo
 def test_modulo():
     a = 2
     b = 3
+    expected = 2
 
-    assert modulo(a, b) == 2
+    result = modulo(a, b)
+
+    assert result == 2, f"Expected {expected}, got {result}" 
+
+
